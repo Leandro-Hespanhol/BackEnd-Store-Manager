652b8940d8ab1b6bd3eb8fac15497a8979870fa9
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-02-26 13:05:41 -0300

    req final

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,44 +1,39 @@
-const { getAllProducts, productsCreate,
-   getById, productToEdit, deletingProduct } = require('../services/productsService');
+const productsService = require('../services/productsService');
 
-const selectAll = async (_req, res) => res.status(200).json(await getAllProducts());
+const selectAll = async (_req, res) => res.status(200).json(await productsService.getAllProducts());
 
 const selectbyId = async (req, res) => {
   const { id } = req.params;
-  const getIdItem = await getById(id);
+  const getIdItem = await productsService.getById(id);
   
   if (!getIdItem) return res.status(404).json({ message: 'Product not found' });
 
   return res.status(200).json(getIdItem);
 };
 
-const createProduct = async (req, res, next) => {
-  try {
+const createProduct = async (req, res) => {
   const { name, quantity } = req.body;
 
-  const allProducts = await getAllProducts();
+  const allProducts = await productsService.getAllProducts();
 
   if (allProducts.some((product) => product.name === name)) {
     return res.status(409).json({ message: 'Product already exists' });
   }
 
-  const newProduct = await productsCreate(name, quantity);
+  const newProduct = await productsService.productsCreate(name, quantity);
 
   return res.status(201).json(newProduct);
-  } catch (e) {
-    next(e);
-  }
 };
 
 const editProduct = async (req, res) => {
   const { name, quantity } = req.body;
   const { id } = req.params;
 
-  const checkExistance = await getById(id);
+  const checkExistance = await productsService.getById(id);
 
   if (!checkExistance) return res.status(404).json({ message: 'Product not found' });
 
-  await productToEdit(id, name, quantity);
+  await productsService.productToEdit(id, name, quantity);
 
   return res.status(200).json({ id, name, quantity });
 };
@@ -46,12 +41,12 @@ const editProduct = async (req, res) => {
 const deleteProduct = async (req, res) => {
   const { id } = req.params;
 
-  const productFound = await getById(id);
+  const productFound = await productsService.getById(id);
 
-  if (await deletingProduct(id) === null) {
+  if (await productsService.deletingProduct(id) === null) {
     return res.status(404).json({ message: 'Product not found' });
   }
-  await deletingProduct(id);
+  await productsService.deletingProduct(id);
   
   return res.status(204).json(productFound);
 };
--- test/unit/controllers/productsControllers.js
+++ test/unit/controllers/productsControllers.js
@@ -1,3 +1,34 @@
 const { expect } = require("chai");
 const sinon = require('sinon');
 
+const productsController = require('../../../controllers/productsController');
+const productsServices = require('../../../services/productsService');
+
+describe('0- Testa controller de producs', () => {
+  describe('1- Verifica a função selectAll', () => {
+    let req = {};
+    let res = {};
+    before(() => {
+      res.status = sinon.stub().returns(res)
+      res.json = sinon.stub().returns([])
+      sinon.stub(productsServices, 'getAllProducts').resolves([])
+    })
+
+    after(() => {
+      productsServices.getAllProducts.restore()
+    })
+
+    it('É chamada resposta com um array', async () => {
+      await productsController.selectAll(req, res)
+
+      expect(res.json.calledWith(sinon.match.array)).to.be.equal(true)
+    })
+
+    it('É chamado status 200', async () => {
+      await productsController.selectAll(req, res)
+      console.log('LINHA21', res.status.calledWith(200));
+
+      expect(res.status.calledWith(200)).to.be.equal(true)
+    })
+  })
+});
